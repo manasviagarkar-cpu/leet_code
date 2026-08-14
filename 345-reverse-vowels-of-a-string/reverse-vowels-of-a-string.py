55b8a0c99de2b4c7f9f3a9e4c8dd7647e734234f
manasviagarkar-cpu <manasviagarkar@gmail.com>
2026-08-14 19:44:08 +0530

    Time: 15 ms (31.69%) | Memory: 20.8 MB (14.51%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -5,13 +5,13 @@ class Solution:
         s = list(s)
         if len(s)==0:
             return s
-        while left < right :
-            if s[left] in vowels and s[right] in vowels :
-                s[right] , s[left]  = s[left] , s[right]
-                left +=1
-                right -=1
-            elif s[left] not in vowels :
-                left +=1
-            elif s[right] not in vowels :
-                right -=1
+        while left < right:
+            while s[left] not in vowels and left < right:
+                left += 1
+            while s[right] not in vowels and left < right:
+                right -= 1
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
         return "".join(s)
