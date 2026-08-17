62498b303f2ed376ae455e862f3bc924374f7e54
manasviagarkar-cpu <manasviagarkar@gmail.com>
2026-08-17 09:44:23 +0530

    Time: 13 ms (69.93%) | Memory: 38.9 MB (31.34%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        f = float('inf')
+        s = float('inf')
+        for i in nums :
+            if i <= f:
+                f = i
+            elif i <= s:
+                s = i 
+            else :
+                return True
+        return False
+        
+        
